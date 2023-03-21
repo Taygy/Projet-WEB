d0552fc5e542b1361441fafd2ff11026582fe0b8
Shosan <97352385+Shosan0@users.noreply.github.com>
2023-03-21 09:27:16 +0100

    ajout de la fonction addComment

--- projetweb/src/controllers/detailentreprise.php
+++ projetweb/src/controllers/detailentreprise.php
@@ -2,7 +2,29 @@
  
 require_once('src/modele/model.php');
 function detailentreprise(string $identifier){
-$entreprise = getEntreprise($identifier);
+    $entreprise = getEntreprise($identifier);
+    $commentaire = getCommentaires($identifier);
+    function addComment(string $identifier, array $input)
+    {
+        $commentaire = null;
+        $note = null;
+        $id_membre = null;
+        $id_entreprise = $identifier;
+        if (!empty($input['commentaire']) && !empty($input['note']) && !empty($input['id_membre'])) {
+            $commentaire = $input['commentaire'];
+            $note = $input['note'];
+            $id_membre = $input['id_membre'];
+        } else {
+            die('Les données du formulaire sont invalides.');
+        }
 
-require('src/templates/detailentreprise.php');
+        $success = createComment($commentaire, $note, $id_membre, $id_entreprise);
+        if (!$success) {
+            die('Impossible d\'ajouter le commentaire !');
+        } else {
+            header('Location: index.php?action=detailentreprise&id='.$identifier);
+        }
+    }
+
+    require('src/templates/detailentreprise.php');
 }
