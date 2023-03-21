b84891a97ff4feea3486cf1c35aba51bd870a8bf
Shosan <97352385+Shosan0@users.noreply.github.com>
2023-03-21 09:26:44 +0100

    ajout de la fonction addComent

--- projetweb/index.php
+++ projetweb/index.php
@@ -27,6 +27,7 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
             $identifier = $_GET['id'];
 
             detailentreprise($identifier);
+            addComment($identifier, $_POST);
         } else {
             echo 'Erreur : aucun identifiant de billet envoyé';
 
