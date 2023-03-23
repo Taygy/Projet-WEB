c09af1f01103e385d9667aa06374a84c48032ada
Thomas Cordier <96497820+Taygy@users.noreply.github.com>
2023-03-24 00:30:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once('src/controllers/detailoffre.php');
+require_once('src/controllers/offre.php');
+require_once('src/controllers/homepage.php');
+require_once('src/controllers/entreprise.php');
+require_once('src/controllers/detailentreprise.php');
+require_once('src/controllers/ajouterentreprise.php');
+require_once('src/controllers/ajouteroffre.php');
+require_once('src/controllers/modifierentreprise.php');
+require_once('src/controllers/apropos.php');
+
+
+if (isset($_GET['action']) && $_GET['action'] !== '') {
+    if ($_GET['action'] === 'offre') {
+        if (isset($_POST['search']) && $_POST['search'] !== '') {
+            $search = $_POST['search'];
+            soffres($search);
+        } else {
+            offre();
+        }
+    } elseif ($_GET['action'] === 'detailoffre') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+
+            detailoffre($identifier);
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+
+            die;
+        }
+    } elseif ($_GET['action'] === 'entreprise') {
+        entreprise();
+    } elseif ($_GET['action'] === 'detailentreprise') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+
+            detailentreprise($identifier);
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+
+            die;
+        }
+    } elseif ($_GET['action'] === 'ajouterentreprise') {
+        addEntreprise($_POST);
+    } elseif ($_GET['action'] === 'homepage') {
+        homepage();
+    } elseif ($_GET['action'] === 'apropos') {
+        apropos();
+    } elseif ($_GET['action'] === 'modifierentreprise') {
+        parse_str(file_get_contents("php://input"), $_PUT);
+        changeEntreprise($_PUT);
+    } elseif ($_GET['action'] === 'ajouteroffre') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+
+            addOffre($_POST, $identifier);
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+
+            die;
+        }
+    }
+} else {
+    homepage();
+}
