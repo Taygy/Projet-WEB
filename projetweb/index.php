86ece65df4037384f4e1c080b5f2cda84eead7e1
Shosan <97352385+Shosan0@users.noreply.github.com>
2023-03-25 16:36:43 +0100

    Add files via upload

--- projetweb/index.php
+++ projetweb/index.php
@@ -6,6 +6,11 @@ require_once('src/controllers/homepage.php');
 require_once('src/controllers/entreprise.php');
 require_once('src/controllers/detailentreprise.php');
 require_once('src/controllers/ajouterentreprise.php');
+require_once('src/controllers/ajouteroffre.php');
+require_once('src/controllers/modifierentreprise.php');
+require_once('src/controllers/apropos.php');
+require_once('src/controllers/supprimerentreprise.php');
+
 
 if (isset($_GET['action']) && $_GET['action'] !== '') {
     if ($_GET['action'] === 'offre') {
@@ -15,7 +20,7 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
         } else {
             offre();
         }
-    }elseif ($_GET['action'] === 'detailoffre') {
+    } elseif ($_GET['action'] === 'detailoffre') {
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             $identifier = $_GET['id'];
 
@@ -25,23 +30,53 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
 
             die;
         }
-	}elseif ($_GET['action'] === 'entreprise'){
-    	entreprise();
-	}
-    elseif ($_GET['action'] === 'detailentreprise'){
+    } elseif ($_GET['action'] === 'entreprise') {
+        entreprise();
+    } elseif ($_GET['action'] === 'detailentreprise') {
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             $identifier = $_GET['id'];
 
             detailentreprise($identifier);
-            addComment($identifier, $_POST);
         } else {
             echo 'Erreur : aucun identifiant de billet envoyé';
 
             die;
         }
-    } elseif ($_GET['action'] === 'ajouterentreprise'){
+    } elseif ($_GET['action'] === 'ajouterentreprise') {
         addEntreprise($_POST);
+    } elseif ($_GET['action'] === 'apropos') {
+        apropos();
+    } elseif ($_GET['action'] === 'modifierentreprise') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+
+            changeEntreprise($_POST, $identifier);
+        } else {
+            echo 'Erreur : CDE aucun identifiant de billet envoyé';
+
+            die;
+        }
+    } elseif ($_GET['action'] === 'deleteentreprise') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+
+            deleteEntreprise($identifier);
+        } else {
+            echo 'Erreur : CDE aucun identifiant de billet envoyé';
+
+            die;
+        }
+    } elseif ($_GET['action'] === 'ajouteroffre') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+
+            addOffre($_POST, $identifier);
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+
+            die;
+        }
     }
 } else {
-	homepage();
+    homepage();
 }
