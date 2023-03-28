4842ea2d0fbe0631aff0eb963b914eda2e516f57
Thomas Cordier <96497820+Taygy@users.noreply.github.com>
2023-03-29 00:08:21 +0200

    Delete projetweb/src/view directory

--- projetweb/src/view/modifieroffre.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('./libs\smarty-master\smarty-master\libs\Smarty.class.php');
-$smarty = new Smarty();
-$smarty->template_dir = './app/view/templates';
-
-$smarty->assign('offre', $offre);
-
-$smarty->display('src/view/templates/modifieroffre.tpl');
--- projetweb/src/view/offre.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('./libs\smarty-master\smarty-master\libs\Smarty.class.php');
-$smarty = new Smarty();
-$smarty->template_dir = './app/view/templates';
-
-$smarty->assign('offres', $offres);
-
-$smarty->display('src/view/templates/offre.tpl');
