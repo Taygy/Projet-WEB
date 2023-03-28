e1c3eec6e165a2f4fbb3d3283ecfc3fa7dedd547
Shosan <97352385+Shosan0@users.noreply.github.com>
2023-03-28 09:41:14 +0200

    Delete pilote.php

--- pilote.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require('./lib/smarty-4.2.1/libs/Smarty.class.php');
-$smarty = new Smarty();
-$smarty->template_dir = './app/view/templates';
-
-$smarty->assign('pilotes', $pilotes);
-
-$smarty->display('src/view/templates/pilote.tpl');
