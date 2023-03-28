d942ba4f63c793ce5bbfb17249f043414eb94a68
Shosan <97352385+Shosan0@users.noreply.github.com>
2023-03-28 09:44:32 +0200

    Add files via upload

--- projetweb/src/view/creerpilote.php
+++ projetweb/src/view/creerpilote.php
@@ -4,6 +4,5 @@ require('./lib/smarty-4.2.1/libs/Smarty.class.php');
 $smarty = new Smarty();
 $smarty->template_dir = './app/view/templates';
 
-$smarty->assign('etudiants', $etudiants);
 
-$smarty->display('src/view/templates/etudiant.tpl');
+$smarty->display('src/view/templates/creerpilote.tpl');
--- projetweb/src/view/pilote.php
+++ projetweb/src/view/pilote.php
@@ -4,6 +4,6 @@ require('./lib/smarty-4.2.1/libs/Smarty.class.php');
 $smarty = new Smarty();
 $smarty->template_dir = './app/view/templates';
 
-$smarty->assign('etudiants', $etudiants);
+$smarty->assign('pilotes', $pilotes);
 
-$smarty->display('src/view/templates/etudiant.tpl');
+$smarty->display('src/view/templates/pilote.tpl');
