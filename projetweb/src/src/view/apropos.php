4db77e6f82f0851fb3e94058eb8ad233f2436824
Shosan <97352385+Shosan0@users.noreply.github.com>
2023-03-29 02:04:00 +0200

    Delete projetweb directory

--- projetweb/src/src/view/apropos.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once('./libs\smarty-master\libs\Smarty.class.php');
-$smarty = new Smarty();
-$smarty->template_dir = './app/view/templates';
-
-
-$smarty->display('src/view/templates/apropos.tpl');
